dd42c69294ccd0c8671fb2710a25548cbde61d91
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-18 12:50:40 +0300

    assignment1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handleButtonClick = () => {
+    setCounter(counter + 1);
+    alert(`You clicked this button ${counter + 1} times`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="https://media.istockphoto.com/id/638460056/photo/single-lion-standing-proudly-on-a-small-hill.jpg?s=612x612&w=0&k=20&c=OyEKBg7dkPcR9wD3fwTUzXJHu7kdxgG3AgdKuMgBMf0=" alt="Lion- King of the jungle" />
+
+        <button onClick={handleButtonClick}>Click Me</button>
+        <p>You clicked this button {counter} times</p>
         <a
           className="App-link"
           href="https://reactjs.org"
